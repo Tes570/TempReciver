444fa33963a19673b4020e7653e27a55ebb99d23
Tes570 <oscar.jr.pantoja@gmail.com>
2020-11-23 10:13:44 -0800

    Set while loop

--- Tes.py
+++ Tes.py
@@ -1,20 +1,19 @@
 import smbus
+import RPi.GPIO as GPIO
 
-#import firebase
-#from firebase import db
 from firebase import firebase as Fire
 
 
-#fireLink = firebase.FirebaseApplication('https://ee175b21.firebaseio.com/')
 fireLink = Fire.FirebaseApplication('https://ee175b21.firebaseio.com/')
 
-#firebase.auth()
-#db = firebase.database()
 
-fireLink.put('EE175b21/user/pi', 'cTemp', str(14))
-fireLink.put('EE175b21/user/pi', 'Hight', str(10))
 
-result = fireLink.get('EE175b21/user/pi/Hight', None)
-#result = ref.child('EE175b21/user/pi').get()
-
-print(result)
+while(1):
+    result = fireLink.get("Demo/Room Setting/room0", None)
+    print(result)
+    print(type(result))
+   
+   
+   
+   
+#fireLink.put('EE175b21/user/pi', 'cTemp', str(14))
