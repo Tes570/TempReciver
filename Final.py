4ebd5f3a1f64bc6ac9b4af873b321b07f7252197
Tes570 <oscar.jr.pantoja@gmail.com>
2021-01-04 22:15:39 -0800

    Fix 2

--- Final.py
+++ Final.py
@@ -4,6 +4,7 @@ import smbus
 from firebase import firebase as Fire
 
 
+fireLink = Fire.FirebaseApplication('https://ee175b21.firebaseio.com//')
 
 GPIO.setmode(GPIO.BCM) 
 
@@ -11,35 +12,36 @@ GPIO.setmode(GPIO.BCM)
 
 class FireData:
 
-	def __init__(self):
-		self.fireLink = Fire.FirebaseApplication('https://ee175b21.firebaseio.com/')
+    def __init__(self):
+	#demo
+        t = 12
 
-	def get(self, room, value):
-		return self.fireLink.get(("Demo//Room Setting//room" + str(room) + "//" + str(value)), None)
+    def get(self, room, value):
+        return fireLink.get(("Demo//Room Setting//room" + str(room) + "//" + str(value)), None)
 
 
 
 
 class PinCon:
 
-	def __init__(self, pin):
-		self.pin = pin
-		GPIO.setup(self.pin, GPIO.OUT)     # Declaring GPIO 21 as output pin
-		self.p = GPIO.PWM(self.pin, 50)     # Created PWM channel at 50Hz frequency
-		self.p.start(2.5)
-		self.ON = False
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(self.pin, GPIO.OUT)     # Declaring GPIO 21 as output pin
+        self.p = GPIO.PWM(self.pin, 50)     # Created PWM channel at 50Hz frequency
+        self.p.start(2.5)
+        self.ON = False
 
-	def set(self, deg):
-		T = (10/180)*deg + 2.5
-		self.p = self.p.ChangeDutyCycle(T)
+    def set(self, deg):
+            T = (10/180)*deg + 2.5
+            self.p = self.p.ChangeDutyCycle(T)
 
-		if deg != 0:
-			self.ON = False
-		else:
-			self.ON = True
+            if deg != 0:
+                    self.ON = False
+            else:
+                    self.ON = True
 
-	def get(self):
-		return self.ON
+    def get(self):
+            return self.ON
 
 
 Pins = [PinCon(16), PinCon(20), PinCon(21)]
@@ -49,7 +51,7 @@ Pins = [PinCon(16), PinCon(20), PinCon(21)]
 try:
     while 1:                    # Loop will run forever
 
-    	print(Pins[0].get())
+        print(Pins[0].get())
     	
 
 # If Keyborad Interrupt (CTRL+C) is pressed
