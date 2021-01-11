33cff8655e2404b3a89f3acc185c8b15f96dd96f
Tes570 <oscar.jr.pantoja@gmail.com>
2021-01-10 18:09:43 -0800

    Final testing

--- Final.py
+++ Final.py
@@ -51,20 +51,20 @@ class PinCon:
 Pins = [PinCon(16), PinCon(20), PinCon(21), PinCon(26)]
 
 OData = FireData(3)
-
-try:
-    while 1:                    # Loop will run forever
-
+while 1:
+    try:
+                        # Loop will run forever
         print(Pins[0].get())
 
         for i in range(OData.Rooms):
-        	if(OData.get_temp(i) > OData.get_setting(i)):
+                if(OData.get_temp(i) > OData.get_setting(i)):
 
-    	
+            
 
-# If Keyborad Interrupt (CTRL+C) is pressed
-except KeyboardInterrupt:
-    pass   # Go to next line
+    # If Keyborad Interrupt (CTRL+C) is pressed
+    except KeyboardInterrupt:
+        #tes
+        pass   # Go to next line
 
 
 GPIO.cleanup()  
