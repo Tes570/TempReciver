cf9109e16beb7f7e3e412e3b23f77a5980c43000
Tes570 <oscar.jr.pantoja@gmail.com>
2021-01-26 15:07:22 -0800

    Fixed Messager.

--- Recive.py
+++ Recive.py
@@ -9,16 +9,39 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(Pin, GPIO.OUT)     # Declaring GPIO 21 as output pin
 
 
+def process(x):
+	GPIO.output(Pin,GPIO.HIGH) 
+	time.sleep(Time)
+	GPIO.output(Pin,GPIO.HIGH) 
+	time.sleep(Time)
+	
+	
+	for i in range(8):
+		if(((x >> i) & 0x01) == 0x01):
+			GPIO.output(Pin,GPIO.HIGH) 
+		else:
+			GPIO.output(Pin,GPIO.LOW) 
+		time.sleep(Time)
+
+	GPIO.output(Pin,GPIO.HIGH) 
+	time.sleep(Time)
+	GPIO.output(Pin,GPIO.LOW) 
+		
+
+def run():
+	
+
+	process(0x35)
+	time.sleep(.000056 * 1800)
+	
+	
+
+
 
 try:
 	while 1:
+		run()
 		
-		GPIO.output(Pin,GPIO.HIGH) 
-
-		time.sleep(Time)
-
-		GPIO.output(Pin,GPIO.LOW) 
-		time.sleep(Time)
 
 		
         
